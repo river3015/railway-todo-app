d4b7bb82d816ea1904a2f6ec14c605b5fb3ba3b5
river3015 <aaxy8402@yahoo.co.jp>
2024-11-30 20:24:02 +0900

    feat: npm scripts設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,12 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: globals.browser }},
+  { ignores: ["node_modules/*"]},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
 ];
-
