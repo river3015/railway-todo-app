20aa07d97ccd21a1fd115c6e2ca81ea9026555ed
river3015 <aaxy8402@yahoo.co.jp>
2024-12-02 19:06:01 +0900

    fix: eslint prettier競合回避のために入れた不要パッケージの削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import typescriptParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { ignores: ["node_modules/*"]},
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { languageOptions: { parser: typescriptParser } },
   eslintConfigPrettier,
 ];
